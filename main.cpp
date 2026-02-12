5a0dbd10bb481fca6d2e1e7b19d0669033accaa2
sprinkuls <maymacler@gmail.com>
2026-02-12 14:53:27 -0500

    add basic theming

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+enum State { work, short_break, long_break };
+
+State state = short_break;
+
+struct Style {
+  Color fg;
+  Color bg;
+};
+
 int main() {
   // Initialization
   //--------------------------------------------------------------------------------------
@@ -14,18 +23,30 @@ int main() {
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
 
-  // timing-related vars, in *seconds*
+  // timing-related vars, in seconds
   float counter = 4;
   float elapsed = 0;
 
-  float work = 25 * 60;       // 25 minutes, work
-  float short_break = 5 * 60; // 5  minutes, short break
-  float long_break = 15 * 60; // 15 minutes, short break
+  const float work = 25 * 60;       // 25 minutes, work
+  const float short_break = 5 * 60; // 5  minutes, short break
+  const float long_break = 15 * 60; // 15 minutes, short break
 
   // how many phases have passed, to determine when should be work vs short vs
   // long timer should be started
   // we 1 index in this household
   int pomodoro_nr = 1;
+
+  // this is temporary i swear it
+  string state = "work";
+
+  Color text_color = BLACK;
+  Color bg_color = MAROON;
+  bool is_paused = false;
+
+  // want to set up pomodoro first, so:
+  // - button to start
+  // - button to pause
+  // - play sound when time up
 
   // the loop is:
   // 1.
@@ -46,45 +67,70 @@ int main() {
   //
   // GOTO begin and repeat.
 
-  cout << "overflow: " << UINT_MAX + 1 << "\n";
-
   // Main loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
     // Update
     //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
+    float delta = GetFrameTime(); // just returns delta time in seconds
+
+    if (!is_paused) {
+      counter -= delta;
+    }
+
+    // prevent from going negative so we can check when the timer has run out
+    int ceiled = max(0.0f, ceil(counter));
+
+    // get number of minutes and seconds from the ceiled seconds
+    int mins = ceiled / 60;
+    int secs = ceiled % 60;
+
+    string min_str = to_string(mins);
+    string sec_str = to_string(secs);
+
+    // pad with a 0 so both values are always two characters wide
+    if (mins < 10) {
+      min_str = "0" + min_str;
+    }
+    if (secs < 10) {
+      sec_str = "0" + sec_str;
+    }
+
+    string time_left = min_str + ":" + sec_str;
+
+    // Update the state based on time left
+    // so it'll be like:
+    //
+    // we check if timer is zero
+    //     if it is, then we set the timer to the amount of time in whatever the
+    //     next cycle is (say, 05:00 if work just ended), and set is_paused to
+    //     true.
+    //
+    // play sound
+
     //----------------------------------------------------------------------------------
 
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
-
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-
-    // literally just delta time, given in seconds rather than milliseconds
-    float delta = GetFrameTime();
-    cout << delta * 60 << "\n";
-    // round the difference (really, take the ceil)
-
-    // yeah this works
-    counter -= delta;
-    int ceiled = ceil(counter);
-    if (ceiled > 0) {
-      DrawText(TextFormat("counter: %i", ceiled), 190, 200, 20, BLACK);
-      elapsed += delta;
+    if (is_paused) {
+      ClearBackground(ColorBrightness(bg_color, -0.1f));
     } else {
-      DrawText("time up!", 190, 200, 20, BLACK);
-      DrawText(TextFormat("elapsed: %f", elapsed), 190, 200, 20, BLACK);
+      ClearBackground(bg_color);
     }
 
-    // want to set up pomodoro first, so:
-    // - button to start
-    // - button to pause
-    // - play sound when time up
+    if (ceiled > 0) {
+      DrawText(TextFormat(time_left.c_str(), ceiled), 190, 200, 20, text_color);
+      elapsed += delta;
+    } else {
+      state = "short_break";
+      text_color = WHITE;
+      bg_color = SKYBLUE;
+
+      DrawText("time up!", 190, 200, 20, text_color);
+      DrawText(TextFormat("elapsed: %f", elapsed), 190, 220, 20, text_color);
+    }
 
     EndDrawing();
     //----------------------------------------------------------------------------------

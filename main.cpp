68d6e8d94c40a5200282a98985c4a94135dfbaa4
sprinkuls <maymacler@gmail.com>
2026-02-12 11:57:12 -0500

    fix off by 1 second error

--- main.cpp
+++ main.cpp
@@ -14,9 +14,41 @@ int main() {
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
 
-  float counter = 10;
+  // timing-related vars, in *seconds*
+  float counter = 4;
+  float elapsed = 0;
 
-  // Main game loop
+  float work = 25 * 60;       // 25 minutes, work
+  float short_break = 5 * 60; // 5  minutes, short break
+  float long_break = 15 * 60; // 15 minutes, short break
+
+  // how many phases have passed, to determine when should be work vs short vs
+  // long timer should be started
+  // we 1 index in this household
+  int pomodoro_nr = 1;
+
+  // the loop is:
+  // 1.
+  // work
+  // short
+  //
+  // 2.
+  // work
+  // short
+  //
+  // 3.
+  // work
+  // short
+  //
+  // 4.
+  // work
+  // long
+  //
+  // GOTO begin and repeat.
+
+  cout << "overflow: " << UINT_MAX + 1 << "\n";
+
+  // Main loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
     // Update
@@ -38,14 +70,21 @@ int main() {
     cout << delta * 60 << "\n";
     // round the difference (really, take the ceil)
 
-    // yeah this works ok
+    // yeah this works
     counter -= delta;
     int ceiled = ceil(counter);
-    if (ceiled >= 0) {
+    if (ceiled > 0) {
       DrawText(TextFormat("counter: %i", ceiled), 190, 200, 20, BLACK);
+      elapsed += delta;
     } else {
       DrawText("time up!", 190, 200, 20, BLACK);
+      DrawText(TextFormat("elapsed: %f", elapsed), 190, 200, 20, BLACK);
     }
+
+    // want to set up pomodoro first, so:
+    // - button to start
+    // - button to pause
+    // - play sound when time up
 
     EndDrawing();
     //----------------------------------------------------------------------------------
